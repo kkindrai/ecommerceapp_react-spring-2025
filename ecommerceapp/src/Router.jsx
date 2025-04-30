6553f9796f9b9a807fc495d3525279737bec7b5e
kendra.jade <kindrai.kendra@gmail.com>
2025-04-30 13:32:23 -0500

    Modified Admin.jsx, Home.jsx, and Router.jsx (Before Debugging)
    Timestamp 1:02:09 commit
    
    https://www.youtube.com/watch?v=RGn5uGP5tMk&ab_channel=PerryGovier

--- ecommerceapp/src/Router.jsx
+++ ecommerceapp/src/Router.jsx
@@ -1,1 +1,21 @@
- 
+import { Routes, Route } from 'react-router-dom'
+
+import Nav from './Nav'
+import Admin from './Admin'
+import Home from './Home'
+import Profile from './Profile'
+
+export default function Router() {
+  
+  return (
+    <Routes>
+        <Route path="/" element={<Nav/>}>
+            <Route index element={<Home/>} />
+            <Route path='/admin' component={<Admin/>} />
+            <Route path='/profile' component={<Profile/>} />
+            <Route path="*" component={<Home/>} />
+        </Route>
+    </Routes>
+        
+  )
+}
